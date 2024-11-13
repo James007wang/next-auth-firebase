50b66e83c1e63b9f2efb9ced0b9d13ae8c509529
James007wang <daoxue.wang@teknion.com>
2024-11-13 14:09:55 -0500

    add firebase config

--- app/firebase.js
+++ app/firebase.js
@@ -6,14 +6,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBFBrwtnWSlBsVyn9JkUPQ8Kv-qCVnAV9A",
+  authDomain: "balmy-platform-423519-j4.firebaseapp.com",
+  projectId: "balmy-platform-423519-j4",
+  storageBucket: "balmy-platform-423519-j4.firebasestorage.app",
+  messagingSenderId: "825233968869",
+  appId: "1:825233968869:web:bbc4c065c3c28b08254e0c",
+  measurementId: "G-H9ST35XH3D"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
